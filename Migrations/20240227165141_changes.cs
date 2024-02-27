935bf18476df9415805a004610f6b702f33e197c
Gudmon <gudmonmarcell@gmail.com>
2024-02-27 17:52:12 +0100

    add: changes electrical floating migration

---
+++ Migrations/20240227165141_changes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CalcAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ElectricalFloating",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Elektromos úszó pozíció emelő és fordító funkcióhoz (A11-A41 vezértömbhöz)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ElectricalFloating",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Elektromos úszó pozíció emelő és fordító funkcióhoz (for A11-A41 vezértömbhöz)");
+        }
+    }
+}
