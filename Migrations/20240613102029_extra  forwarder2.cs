c4f4984eec8de66395a9225cdf10bd76c7ad8610
Gudmon <gudmonmarcell@gmail.com>
2024-06-18 20:28:13 +0200

    add: extra forwarder stanchions

---
+++ Migrations/20240613102029_extra  forwarder2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CalcAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class extraforwarder2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "KrpanTrailer",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "KrpanExtraForwarderStanchionId",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "KrpanTrailer",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "KrpanExtraForwarderStanchionId",
+                value: 1);
+        }
+    }
+}
