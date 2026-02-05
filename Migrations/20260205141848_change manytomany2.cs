4a8b984d7e36741e656cf3194adbfa22561398a3
Gudmon <gudmonmarcell@gmail.com>
2026-02-05 15:19:26 +0100

    fix: add migration

---
+++ Migrations/20260205141848_change manytomany2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CalcAppAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class changemanytomany2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BrakeTrailer",
+                columns: table => new
+                {
+                    BrakeId = table.Column<int>(type: "int", nullable: false),
+                    TrailerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BrakeTrailer", x => new { x.BrakeId, x.TrailerId });
+                    table.ForeignKey(
+                        name: "FK_BrakeTrailer_Brake_BrakeId",
+                        column: x => x.BrakeId,
+                        principalTable: "Brake",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BrakeTrailer_Trailer_TrailerId",
+                        column: x => x.TrailerId,
+                        principalTable: "Trailer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BrakeTrailer_TrailerId",
+                table: "BrakeTrailer",
+                column: "TrailerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BrakeTrailer");
+        }
+    }
+}
